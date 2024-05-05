6e2a5161a96fd3f065284c0ddfd0eb50751e4e39
Pedro Henrique <pedrodguimaraes20@gmail.com>
2024-05-05 14:23:03 -0300

    Reimplement findByLocation method

--- src/main/java/com/example/runnerz/run/RunRepository.java
+++ src/main/java/com/example/runnerz/run/RunRepository.java
@@ -1,7 +1,11 @@
 package com.example.runnerz.run;
 
+import java.util.List;
+
 import org.springframework.data.repository.ListCrudRepository;
 
 public interface RunRepository extends ListCrudRepository<Run,Integer>{
+
+    List<Run> findAllByLocationn(String location);
 
 }
